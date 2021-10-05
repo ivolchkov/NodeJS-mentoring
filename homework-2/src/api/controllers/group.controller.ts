3121c5d9377b2d7938d168c7dad35a8ddf08c6dd
ivolchkov <igorik32506632@gmail.com>
2021-10-05 13:37:14 +0300

    [HW-5] - Implemented decorator for measuring execution time for each method

--- homework-2/src/api/controllers/group.controller.ts
+++ homework-2/src/api/controllers/group.controller.ts
@@ -1,6 +1,7 @@
 import { Inject, Service } from 'typedi';
 import GroupService from '../../services/group.service';
 import { GroupDTO, GroupModel } from '../../models/Group';
+import { Measure } from '../middlewares/execution.time.measure';
 
 @Service()
 export default class GroupController {
@@ -8,22 +9,27 @@ export default class GroupController {
         this.groupService = groupService;
     }
 
+    @Measure
     public createGroup(group: GroupDTO): Promise<GroupModel | Error> {
         return this.groupService.create(group);
     }
 
+    @Measure
     public getById(id: string): Promise<GroupModel> {
         return this.groupService.getById(id);
     }
 
+    @Measure
     public findAll(): Promise<Array<GroupModel>> {
         return this.groupService.findAll();
     }
 
+    @Measure
     public updateGroup(id: string, group: GroupDTO): Promise<GroupModel> {
         return this.groupService.update(id, group);
     }
 
+    @Measure
     public deleteGroup(id: string): Promise<number> {
         return this.groupService.delete(id);
     }
--- homework-2/src/api/controllers/user.controller.ts
+++ homework-2/src/api/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { Inject, Service } from 'typedi';
 import UserService from '../../services/user.service';
 import { UserDTO, UserModel } from '../../models/User';
 import { UserGroupModel } from '../../models/UserGroup';
+import { Measure } from '../middlewares/execution.time.measure';
 
 @Service()
 export default class UserController {
@@ -9,26 +10,32 @@ export default class UserController {
         this.userService = userService;
     }
 
+    @Measure
     public createUser(user: UserDTO): Promise<UserModel | Error> {
         return this.userService.create(user);
     }
 
+    @Measure
     public addUsersToGroup(groupId: string, userIds: Array<string>): Promise<Array<UserGroupModel>> {
         return this.userService.addUsersToGroup(groupId, userIds);
     }
 
+    @Measure
     public getById(id: string): Promise<UserModel> {
         return this.userService.getById(id);
     }
 
+    @Measure
     public getByLoginSubstringAndLimitSize(loginSubstring: string, limit: number): Promise<Array<UserModel>> {
         return this.userService.getByLoginSubstringAndLimitSize(loginSubstring, limit);
     }
 
+    @Measure
     public updateUser(id: string, user: UserDTO): Promise<UserModel> {
         return this.userService.update(id, user);
     }
 
+    @Measure
     public deleteUser(id: string): Promise<UserModel> {
         return this.userService.delete(id);
     }
