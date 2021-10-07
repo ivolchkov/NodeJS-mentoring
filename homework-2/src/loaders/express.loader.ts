2e7df940e5db7561f962fd2fd80bcc59e411206d
ivolchkov <igorik32506632@gmail.com>
2021-10-07 18:38:35 +0300

    [HW-6] - Add CORS middleware to access service methods from WEB applications hosted on another domains

--- homework-2/src/loaders/express.loader.ts
+++ homework-2/src/loaders/express.loader.ts
@@ -1,16 +1,20 @@
 import express, { Application } from 'express';
 import userRouter from '../api/routers/user.router';
 import groupRouter from '../api/routers/group.router';
+import authRouter from '../api/routers/auth.router';
 import { ContainerDependencies } from './dependency.injector.loader';
 import unhandledErrorLogging from '../api/middlewares/unhandled.error.logging';
+import cors from 'cors';
 
 export default (containerDependencies: ContainerDependencies): Application => {
     const app = express();
 
     app.use(express.json());
+    app.use(cors());
 
-    app.use('/api/v1/users', userRouter(containerDependencies.userController, containerDependencies.logger));
-    app.use('/api/v1/groups', groupRouter(containerDependencies.groupController, containerDependencies.logger));
+    app.use('/api/v1/users', userRouter(containerDependencies));
+    app.use('/api/v1/groups', groupRouter(containerDependencies));
+    app.use('/api/v1/auth', authRouter(containerDependencies));
 
     app.use(unhandledErrorLogging(containerDependencies.logger));
 
